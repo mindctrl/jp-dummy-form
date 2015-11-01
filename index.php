e7ad8f370348259637fa8dac867038ebedeb5623
John Parris <public@johnparris.com>
2015-10-31 21:03:04 -0400

    div -> p

--- index.php
+++ index.php
@@ -6,63 +6,65 @@
  * Author: John Parris
  * Author URI: https://www.johnparris.com
  */
-function jp_dummy_form( $atts, $content ) {
+function jp_dummy_form( $atts, $content = null ) {
 	?>
 
 	<div class="dummy-form-wrap">
 		<form action="#">
 			<fieldset>
-				<legend>Legend</legend>
+				<p class="field-wrap">
+					<legend>Legend</legend>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="optional-text">Optional Text input:</label>
 					<input type="text" id="optional-text" value="input text" />
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="required-text">Required Text input:</label>
 					<input type="text" id="required-text" required />
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="twp">Text input with pattern requirement and placeholder:</label>
 					<input type="text" id="twp" pattern="\d{5}(-\d{4})?" title="a US Zip code, with or without the +4 exension" placeholder="12345-6789"/>
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="search">Search input:</label>
 					<input type="search" id="search" />
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="email">Email input:</label>
 					<input type="email" id="email" />
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="url">URL input:</label>
 					<input type="url" id="url" />
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="password">Password input:</label>
 					<input type="password" id="password" value="password" />
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label>Radio input:</label>
 					<input type="radio" id="radio1" name="radio" value="option1" />
 					<label for="radio1">Option 1</label>
 					<input type="radio" id="radio2" name="radio" value="option2" />
 					<label for="radio2">Option 2</label>
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="checkbox">Checkbox input:</label>
 					<input type="checkbox" id="checkbox" />
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="multi-checkbox">Multiple checkbox:</label>
 					<input type="checkbox" id="multi-checkbox1" name="multi-checkbox[]" value="option1">
 					<label for="multi-checkbox1">Option 1</label>
@@ -70,90 +72,90 @@ function jp_dummy_form( $atts, $content ) {
 					<label for="multi-checkbox1">Option 2</label>
 					<input type="checkbox" id="multi-checkbox3" name="multi-checkbox[]" value="option3">
 					<label for="multi-checkbox1">Option 3</label>
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="select">Select field:</label>
 					<select id="select">
 						<option>Option 1</option>
 						<option>Option 2</option>
 					</select>
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="textarea">Textarea:</label>
 					<textarea id="textarea" cols="30" rows="5">Textarea text</textarea>
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="color">Color:</label>
 					<input type="color" name="color">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="file">File:</label>
 					<input type="file" name="file" id="file" required="required">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="number">Number:</label>
 					<input type="number" name="number" id="number" required="required">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="date">Date:</label>
 					<input type="date" name="date">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="datetime">Date & Time:</label>
 					<input type="datetime" name="datetime">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="datetime-local">Local Date & Time:</label>
 					<input type="datetime-local" name="datetime-local">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="month">Month:</label>
 					<input type="month" name="month">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="range">Range:</label>
 					<input type="range" id="range" name="range">
 					<output for="range" id="output"></output>
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="time">Time:</label>
 					<input type="time" name="time">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="week">Week:</label>
 					<input type="week" name="week">
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="phone">Phone input:</label>
 					<input id="phone" name="phone" type="tel" placeholder="e.g. 8005551212" required>
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="button1">Button:</label>
 					<input type="button" id="button1" value="Button Text" />
-				</div>
+				</p>
 
-				<div class="field-wrap">
+				<p class="field-wrap">
 					<label for="submit-button">Submit:</label>
 					<input type="submit" id="submit-button" value="Button Text" />
-				</div>
+				</p>
 			</fieldset>
 		</form>
 	</div>
 
 	<?php
 }
-add_shortcode( 'dummy_form', 'jp_dummy_form', 10, 2 );
+add_shortcode( 'dummy_form', 'jp_dummy_form' );
